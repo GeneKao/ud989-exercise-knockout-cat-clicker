b2d7cbbdd323f0c1fd2665b32de91850ab277414
Gene Kao <kao.gene@gmail.com>
2018-05-01 17:06:20 +0200

    Add abservableArray, observableComputed in model and foreach in view.

--- js/app.js
+++ js/app.js
@@ -6,6 +6,27 @@ var ViewModel = function() {
     this.name = ko.observable('Tabby');
     this.imgSrc = ko.observable('img/434164568_fea0ad4013_z.jpg');
     this.imgAttribution = ko.observable('https://www.flickr.com/photos/malfet/1413379559');
+    this.nicknames = ko.observableArray(['Tabtab', 'T-Bone', 'Mr. T',
+                                         'Tabitha Tab Tabby Catty Cat']);
+
+    this.title = ko.computed(function() {
+        var title;
+        var clicks = this.clickCount();
+        if (clicks < 10 ) {
+            title = 'Newborn';
+        } else if (clicks < 50) {
+            title = 'Infant';
+        } else if (clicks < 100) {
+            title = 'Child';
+        } else if (clicks < 200) {
+            title = 'Teen';
+        } else if (clicks < 500) {
+            title = 'Adult';
+        } else {
+            title = 'Ninja';
+        }
+        return title;
+    }, this);
 
     this.incrementCounter = function() {
         this.clickCount(this.clickCount() + 1);
