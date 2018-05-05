77a9ad01c672e1a98908594e2155be8735a24d48
Gene Kao <kao.gene@gmail.com>
2018-05-05 11:46:02 +0200

    getting ready to add more cats

--- js/app.js
+++ js/app.js
@@ -1,13 +1,12 @@
 // for getting the data from google chrome console.
 // https://stackoverflow.com/questions/15310659/using-chrome-console-to-access-knockout-viewmodel-with-requirejs
 
-var Cat = function() {
-    this.clickCount = ko.observable(0);
-    this.name = ko.observable('Tabby');
-    this.imgSrc = ko.observable('img/434164568_fea0ad4013_z.jpg');
-    this.imgAttribution = ko.observable('https://www.flickr.com/photos/malfet/1413379559');
-    this.nicknames = ko.observableArray(['Tabtab', 'T-Bone', 'Mr. T',
-                                         'Tabitha Tab Tabby Catty Cat']);
+var Cat = function(data) {
+    this.clickCount = ko.observable(data.clickCount);
+    this.name = ko.observable(data.name);
+    this.imgSrc = ko.observable(data.imgSrc);
+    this.imgAttribution = ko.observable(data.imgAttribution);
+    this.nicknames = ko.observableArray(data.nicknames);
 
     this.title = ko.computed(function() {
         var title;
@@ -32,10 +31,18 @@ var Cat = function() {
 
 var ViewModel = function() {
 
-    this.currentCat = ko.observable( new Cat() );
+    var self = this;
+
+    this.currentCat = ko.observable( new Cat({
+        clickCount: 0,
+        name: 'Tabby',
+        imgSrc: 'img/434164568_fea0ad4013_z.jpg',
+        imgAttribution: 'https://www.flickr.com/photos/malfet/1413379559',
+        nicknames: ['Tabtab', 'T-Bone', 'Mr. T', 'Tabitha Tab Tabby Catty Cat']
+    }) );
 
     this.incrementCounter = function() {
-        this.currentCat().clickCount(this.currentCat().clickCount() + 1);
+        self.currentCat().clickCount(self.currentCat().clickCount() + 1);
 
         var count = 0;
         count++;
